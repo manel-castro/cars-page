10e1303c18da2f13710a560c7517f55e98bfc952
mane936 <53373350+mane936@users.noreply.github.com>
2023-06-22 21:37:18 +0200

    boilerplate

---
+++ src/app/components/navbar/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { styled } from "styled-components";
+import tw from "twin.macro";
+
+const NavbarContainer = styled.div`
+  min-height: 68pw;
+  ${tw`
+    w-full
+    max-width-2xl
+  `}
+`;
